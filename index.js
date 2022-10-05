6cd0a9811b46e660fb2e74f4361f6ba3079ecdb0
dewittd14 <deanna.dewitt14@gmail.com>
2022-10-05 19:48:59 -0400

    finnished input array

--- index.js
+++ index.js
@@ -26,7 +26,44 @@ function init(){
 		name: 'managerOfficeNumber',
 		message: 'what is the office number?'
 	},
-	]
-
-	)
+	{
+		type: 'loop',
+		name: 'team',
+		message: 'Want to add another team memeber?',
+		questions: [
+			{
+			type: 'list',
+			name: 'employeeType',
+			choices: ['Engineer','Intern']
+			},
+			{
+			type: 'input',
+			name: 'employeeName',
+			message: 'What is the employees name?',
+			},
+			{
+			type: 'input',
+			name: 'employeeID',
+			message: 'What is the employees ID?'
+			},
+			{
+			type: 'input',
+			name: 'employeeEmail',
+			message: 'What is the employees email address?',
+			},
+			{
+				type: 'input',
+				name: 'githubUN',
+				message: 'What is employees GitHub username?',
+				when: (team) => team.employeeType === 'Engineer'
+			},
+			{
+				type: 'input',
+				name: 'internSchool',
+				message: 'What is the name of Interns School?',
+				when: (team) => team.employeeType === 'Intern'
+			},
+		],
+	}
+	])
 }
