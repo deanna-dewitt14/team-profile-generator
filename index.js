9bd3cf69802fa775072f35adc0232f4103197ab5
dewittd14 <deanna.dewitt14@gmail.com>
2023-01-06 22:01:01 -0500

    fixed error that wasn't allowing engineer.test to run without failing

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateHtml = require('./dist/index.html');
 inquirer.registerPrompt('loop', require('inquirer-loop')(inquirer));
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
@@ -8,7 +7,7 @@ const Intern = require('./lib/intern');
 
 const questions = [];
 
-function init(){
+/* function init(){
 	return inquirer
 	.prompt([
 	{
@@ -85,13 +84,12 @@ function init(){
 				interns.push(intern);
 			}
 		});
-		
-		var managerCard = generateManagerCard(manager);
-		var engineerCard = generateEngineerCard(engineer);
-		var internCard = generateInternCard(intern)
-		var html = generateHtml(managerCard, engineerCard, internCard)
+	var managerCard = generateManagerCard(manager);
+	var engineerCard = generateEngineerCard(engineer);
+	var internCard = generateInternCard(intern);
+	var html = generateHtml(managerCard, engineerCard, internCard);
 
-		writeToFile(html);
+	writeToFile(html);
 	})
 }
 function writeToFile(html) {
@@ -99,6 +97,103 @@ function writeToFile(html) {
 		if (err) throw err;
 		console.log("Creating Team!");
 });	
+}
+
+init(); */ 
+
+function init() {
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'managerName',
+        message: "What is the manager's name?",
+      },
+      {
+        type: 'input',
+        name: 'id',
+        message: "What is the manager's employee ID number?",
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: "What is the manager's email address?",
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: "What is the manager's office number?",
+      },
+      {
+        type: 'loop',
+        name: 'team',
+        message: 'Add another team member?',
+        questions: [
+            {
+                type: 'list',
+                name: 'employeeType',
+                choices: ['Engineer', 'Intern']
+            },
+            {
+                type: 'input',
+                name: 'employeeId',
+                message: 'What is the employees ID?',
+            },
+            {
+                type: 'input',
+                name: 'employeeName',
+                message: 'What is the employees name?',
+
+            },
+            {
+                type: 'input',
+                name: 'employeeEmail',
+                message: 'What is the employees email address?',
+            },
+            {
+                type: 'input',
+                name: 'githubUsername',
+                message: 'What is the employees GitHub username?',
+                when: (teamMember) => teamMember.employeeType === 'Engineer'
+            },
+            {
+                type: 'input',
+                name: 'internSchool',
+                message: 'What is the name of the employees school?',
+                when: (teamMember) => teamMember.employeeType === 'Intern'
+            },
+        ],
+      }
+    ])
+    .then(projectData => {
+        var manager = new Manager(projectData.managerName, projectData.id, projectData.email, projectData.officeNumber);
+        var engineers = [];
+        var interns = [];
+
+        projectData.team.forEach(function(e) {
+            if (e.employeeType === 'Engineer') {
+                const engineer = new Engineer(e.employeeName, e.employeeId, e.employeeEmail, e.githubUsername);
+                engineers.push(engineer);
+            } else if (e.employeeType === 'Intern') {
+                const intern = new Intern(e.employeeName, e.employeeId, e.employeeEmail, e.internSchool);
+                interns.push(intern);
+            }
+        });
+
+        var managerCard = generateManagerCard(manager);
+        var engineersCard = generateEngineersCard(engineers);
+        var internsCard = generateInternsCard(interns);
+        var html = generateHtml(managerCard, engineersCard, internsCard);
+
+        writeToFile(html);
+    })
+}
+
+function writeToFile(html) {
+    fs.writeFile('index.html', html, function (err) {
+        if (err) throw err;
+        console.log('Generating Team Members...');
+    });
 }
 
 init();
